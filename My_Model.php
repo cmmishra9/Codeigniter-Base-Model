b6b106c48ff4c71f8213a3b9bba28fca64dc1e11
Chandan Mishra <cmmishra9@users.noreply.github.com>
2023-07-01 18:16:23 +0530

    Update My_Model.php from ci 3 to ci 4
    Update My_Model.php from ci 3 to ci 4

--- My_Model.php
+++ My_Model.php
@@ -1,61 +1,61 @@
 <?php
-class MY_Model extends CI_Model {
-	
-	protected $_table_name=null;
 
-	protected $_primary_key=null;
+namespace App\Models;
 
-	public function __construct() {
-		parent:: __construct();
-	}
-	
-	public function get($id=null, $order_by= null){
-	   
-		
-		if(is_numeric($id)){
-           $q=$this->db->where($this->_primary_key, $id);
-		}
-		if(is_array($id)){
-			foreach($id as $_key=>$_value){
-				$this->db->where($_key, $_value);
-			}
-		}
+use CodeIgniter\Model;
 
-		$q=$this->db->get($this->_table_name);
-		return $q->result_array();
-	}
+class MY_Model extends Model
+{
+    protected $table = null;
 
+    protected $primaryKey = null;
 
-	public function insert($data){
-	    $this->db->insert($this->_table_name, $data);
-	    return $this->db->insert_id();
-    }
+    protected $allowedFields = [];
 
-    public function update($new_data, $where){
-        if(is_numeric($where)){
-            $this->db->where($this->_primary_key, $where);
-        }elseif(is_array($where)){
-           foreach($where as $_key=>$_value){
-               $this->db->where($_key, $_value);
-		   }
-        }else{
-			die("You must pass the second Parameter to UPDATE() method.");
-		}
-	    $this->db->update($this->_table_name, $new_data);
-	    return $this->db->affected_rows();
-    }
-
-    public function delete($id){
-	    if(is_numeric($id)){
-	        $this->db->where($this->_primary_key, $id);
-        } elseif(is_array($id)){
-            foreach($id as $_key=>$_value){
-                $this->db->where($_key, $_value);
-            }
-        }else {
-	        die("You must pass a parameter to the DELETE() method");
+    public function get($id = null, $order_by = null)
+    {
+        if (is_numeric($id)) {
+            $this->where($this->primaryKey, $id);
         }
-	    $this->db->delete($this->_table_name);
-	    return $this->db->affected_rows();
+        if (is_array($id)) {
+            $this->where($id);
+        }
+
+        $query = $this->getWhere($order_by);
+        return $query->getResultArray();
+    }
+
+    public function insert($data)
+    {
+        $this->insert($data);
+        return $this->insertID();
+    }
+
+    public function update($new_data, $where)
+    {
+        if (is_numeric($where)) {
+            $this->where($this->primaryKey, $where);
+        } elseif (is_array($where)) {
+            $this->where($where);
+        } else {
+            die("You must pass the second Parameter to the update() method.");
+        }
+
+        $this->update($new_data);
+        return $this->affectedRows();
+    }
+
+    public function delete($id)
+    {
+        if (is_numeric($id)) {
+            $this->where($this->primaryKey, $id);
+        } elseif (is_array($id)) {
+            $this->where($id);
+        } else {
+            die("You must pass a parameter to the delete() method.");
+        }
+
+        $this->delete();
+        return $this->affectedRows();
     }
 }
